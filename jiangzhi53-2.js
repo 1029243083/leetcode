7b877289f17f3beaf61ee121dd14769694d327b4
szc0925 <51327454+1029243083@users.noreply.github.com>
2021-11-07 21:28:30 +0800

    jianzhi53-2-ok

---
+++ jiangzhi53-2.js
@@ -0,0 +1,36 @@
+/**
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+
+示例 1:
+
+输入: [0,1,3]
+输出: 2
+示例 2:
+
+输入: [0,1,2,3,4,5,6,7,9]
+输出: 8
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    let i = 0;
+    let index = 0;
+    while (true) {
+        if (nums[index] == i) {
+            index ++;
+            i ++;
+        }else{
+            console.log(index)
+            break
+        }
+    }
+    return index;
+};
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 7, 9]))
